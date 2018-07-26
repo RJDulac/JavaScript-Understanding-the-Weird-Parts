5bbb3f46c7ae4c62639a0f91ff9a52666dbb622e
Ryan Dulac <analyticrye@gmail.com>
2018-07-25 23:49:16 -0400

    none

--- D20_FunctionalProgramming/Starter/app.js
+++ D20_FunctionalProgramming/Starter/app.js
@@ -20,9 +20,35 @@ var arr2 = mapForEach(arr1, function(item) {
 
 
 
-var arr3 = mapForEach(arr2, function(item) {
-	return item > 3;
+var arr3 = mapForEach(arr1, function(item) {
+	return item > 2;
 
 });
 
 console.log(arr2, arr3);
+
+
+
+
+//function expression that has a limiter to check against the item
+var checkPastLimit = function(limiter, item) {
+	return item > limiter;
+}
+
+// var checkBound = function(limiter) {
+// 	checkPastLimit.bind(this, limiter); 
+// }
+//copy of checkPastLimit function with 1 binded to limiter
+// var arr4 = mapForEach(arr, checkBound(1));
+
+// console.log(arr4);
+
+var checkPastLimiteSimplified = function(limiter) {
+	return function(limiter, item) {
+		return item > limiter;
+	}.bind(this, limiter)
+};
+
+var arr5 = mapForEach(arr1, checkPastLimiteSimplified(2));
+
+console.log(arr5);
